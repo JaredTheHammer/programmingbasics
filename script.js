b7d79d9fa659ed07c1ca5a6750e444691a4c9438
JaredTheHammer <jared.m.hamm@gmail.com>
2023-06-30 08:46:16 -1000

    gradingapp-gptversion

--- script.js
+++ script.js
@@ -1,38 +1,88 @@
-// Add your JavaScript code here
-console.log("Hello, World!");
-let name = "Jared Hamm";
-console.log(name);
+// Javascript code starts here
+    // Define grading thresholds
+    const grades = [
+        {threshold: 0, grade: 'F'},
+        {threshold: 60, grade: 'D-'},
+        {threshold: 63, grade: 'D'},
+        {threshold: 67, grade: 'D+'},
+        {threshold: 70, grade: 'C-'},
+        {threshold: 73, grade: 'C'},
+        {threshold: 77, grade: 'C+'},
+        {threshold: 80, grade: 'B-'},
+        {threshold: 83, grade: 'B'},
+        {threshold: 87, grade: 'B+'},
+        {threshold: 90, grade: 'A-'},
+        {threshold: 93, grade: 'A'},
+        {threshold: 97, grade: 'A+'}
+    ];
 
-function add(a, b){
-    return a+b
-}
+    // Define error and success messages
+    const ERR_INVALID_GRADE = 'The grade entered is not valid. Please enter a grade between 1 and 100.';
+    const ERR_GRADE_NOT_FOUND = 'The grade could not be determined. Please check your input.';
+    const MSG_GRADE_RESULT = 'Your letter grade is ';
 
-let sum = add(2, 5);
+    // Get DOM elements
+    const gradeInput = document.getElementById('grade');
+    const outputMessage = document.getElementById('output-message');
+    const form = document.getElementById('grading-form');
 
-console.log(sum);
-
-let person = {
-    name: "Jared",
-    age: 37,
-    talks: function (words){
-        console.log(`Hello, how are you, ${words}`)
+    // Function to validate input grade
+    const validateGrade = (grade) => {
+        if (grade === '' || isNaN(grade) || grade < 1 || grade > 100) {
+            gradeInput.value = '';
+            return ERR_INVALID_GRADE;
+        }
+        return null;
     }
-}
+
+    // Function to update output message
+    function setState(newState) {
+        if (newState.hasOwnProperty('error')) {
+            outputMessage.innerText = newState.error;
+            outputMessage.className = 'output-message error';
+            outputMessage.style.opacity = newState.error ? '1' : '0';
+            outputMessage.style.display = newState.error ? 'block' : 'none';
+        }
+        if (newState.hasOwnProperty('success')) {
+            outputMessage.innerText = newState.success;
+            outputMessage.className = 'output-message success';
+            outputMessage.style.opacity = newState.success ? '1' : '0';
+            outputMessage.style.display = newState.success ? 'block' : 'none';
+        }
+    }
 
 
-// Add your JavaScript code here
-console.log("Hello, World!");
+    // Add event listener to form submit
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-// Get the current date
-var currentDate = new Date();
+        const grade = Number(gradeInput.value);
+        const error = validateGrade(grade);
 
-// Format the date as desired (e.g., "June 24, 2023")
-var formattedDate = currentDate.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric"
-});
+        if (error) {
+            setState({ error });
+        } else {
+            const result = grades.reverse().find(gradeInfo => grade >= gradeInfo.threshold);
+            grades.reverse(); // revert to original order
+            if (result) {
+                setState({ success: MSG_GRADE_RESULT + result.grade });
+                gradeInput.value = '';
+            } else {
+                gradeInput.value = '';
+                setState({ error: ERR_GRADE_NOT_FOUND });
+            }
+        }
+    });
 
-// Find the element with the id "date" in the HTML and update its content
-var dateElement = document.getElementById("date");
-dateElement.textContent = formattedDate;
+    // Add event listener to reset button
+    document.getElementById('reset-button').addEventListener('click', function(event) {
+        // Prevent the form from resetting with the browser's default action
+        event.preventDefault();
+
+        // Manually clear the input field
+        gradeInput.value = '';
+
+        // Clear the output message
+        outputMessage.innerText = '';
+        outputMessage.style.opacity = '0';
+    });
